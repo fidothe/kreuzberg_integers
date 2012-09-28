50a7b73746f575f16d1da6aa1db9ff4364319116
Matt Patterson <matt@reprocessed.org>
2012-09-28 17:55:34 +0200

    Restart after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,9 @@ server ENV['CAP_SERVER'], :web, :app, :db, :primary => true
 # after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+after "deploy:update", "deploy:restart"
